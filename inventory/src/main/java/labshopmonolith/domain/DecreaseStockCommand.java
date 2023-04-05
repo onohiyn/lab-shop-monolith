78fe9c5c39e86881650a9e49d1f1126346f3eff6
MoonHW <60498178+onohiyn@users.noreply.github.com>
2023-04-05 09:36:00 +0900

    commit

---
+++ inventory/src/main/java/labshopmonolith/domain/DecreaseStockCommand.java
@@ -0,0 +1,14 @@
+package labshopmonolith.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+public class DecreaseStockCommand {
+
+    @Id
+    //@GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer qty;
+}
